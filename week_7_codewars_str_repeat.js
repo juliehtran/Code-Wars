0e0c8951c64db651c1a37f6f63329b240d34543b
Julie Tran <tranjhdesign@gmail.com>
2021-02-25 17:34:59 -0500

    Wrote a function called repeat_str which repeats the given string src exactly count times.

---
+++ week_7_codewars_str_repeat.js
@@ -0,0 +1,7 @@
+// Write a function called repeat_str which repeats the given string
+// src exactly count times.
+
+function repeatStr (count, src) {
+  let str = src.repeat(count);
+  return str
+}
