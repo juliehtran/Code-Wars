ef2e4068c582515626aa9cbd3674e817607c1d2b
Julie Tran <tranjhdesign@gmail.com>
2021-03-28 16:10:19 -0400

    7 kyu list filtering

---
+++ 7_kyu_list_filtering.js
@@ -0,0 +1,14 @@
+// In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+
+
+function filter_list(l) {
+  return l.filter(function(v) {
+      return typeof v == 'number'
+      })
+}
+
+or 
+
+function filter_list(l) {
+  return l.filter( v => typeof v == 'number')
+}
