3886a521498cccf42819c53a4628cbb68233063e
Julie Tran <tranjhdesign@gmail.com>
2021-04-12 23:29:21 -0400

    edited without splice

--- 7_kyu_AlternateCase.js
+++ 7_kyu_AlternateCase.js
@@ -2,14 +2,14 @@
 
 function alternateCase(s) {
     let arr = s.split('')
-    for (let i = 0; i < arr.length; i++){
-      
-      if (arr[i] === arr[i].toUpperCase()){
-       arr.splice(i,1,arr[i].toLowerCase())
-        
-      }else{
-        arr.splice(i,1,arr[i].toUpperCase())
-      }
+    for (let i = 0; i < arr.length; i++) {
+
+        if (arr[i] === arr[i].toUpperCase()) {
+            arr[i] = arr[i].toLowerCase()
+
+        } else {
+            arr[i] = arr[i].toUpperCase()
+        }
     }
     return arr.join('')
-  }
+}
