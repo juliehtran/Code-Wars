54e43522d6e34044e8cd839cf129af5258d36501
Julie Tran <tranjhdesign@gmail.com>
2021-04-19 20:46:09 -0400

    increase the vowel count when given a string using loop and if statement

---
+++ 7_kyu_vowel_counter.js
@@ -0,0 +1,18 @@
+// Return the number (count) of vowels in the given string.
+
+// We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+// The input string will only consist of lower case letters and/or spaces.
+
+function getCount(str) {
+  let vowelsCount = 0;
+  const vowel = ["a", "e", "i", "o", "u"]
+  
+  for(let letter of str.toLowerCase()){
+    if(vowel.includes(letter)){
+      vowelsCount++
+    }
+  }
+  
+  return vowelsCount;
+}
