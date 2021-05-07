d6fabd5f13a2063e2f964ebcf587a3032a57af81
Julie Tran <tranjhdesign@gmail.com>
2021-05-06 21:13:59 -0400

    return the middle character of a word w/ math.floor and conditionals

---
+++ 7_kyu_middle_character.js
@@ -0,0 +1,16 @@
+// You are going to be given a word. Your job is to return the middle character of the word. 
+// If the word's length is odd, return the middle character. If the word's length is even, 
+// return the middle 2 characters.
+
+
+
+function getMiddle(s){
+  let length = s.length;
+  let middle = Math.floor(length/2);
+  
+  if(s.length % 2 === 0){  
+    return s[middle - 1] + s[middle]
+  } else {
+    return s[middle]
+  }
+}
